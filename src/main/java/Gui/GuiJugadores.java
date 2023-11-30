7e172ae5d2b0f75072a706decf9a5ae99b108c21
jfaundez07 <j.faundez07@ufromail.cl>
2023-11-30 09:28:26 -0300

    set up ventana selecciones

--- src/main/java/Gui/GuiJugadores.java
+++ src/main/java/Gui/GuiJugadores.java
@@ -1,4 +1,22 @@
 package Gui;
 
-public class GuiJugadores {
+import javax.swing.*;
+
+public class GuiJugadores extends JFrame{
+    private JPanel jugadoresPanel;
+    private JLabel informacíonLabel;
+    private JButton editarButton;
+    private JButton guardarButton;
+    private JButton volverButton;
+    private JTextArea jugadoresTextArea;
+
+    public GuiJugadores(){
+        setUpGui();
+    }
+
+    private void setUpGui(){
+
+    }
+
+
 }
